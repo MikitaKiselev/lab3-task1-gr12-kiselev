7719b376fe54ee95b9e812ab10c5e29e98fd7cf1
Mikita Kiselev <nikitakiselev557@gmail.com>
2024-03-13 15:35:25 +0300

    Update hello.c

--- src/hello.c
+++ src/hello.c
@@ -1,8 +1,33 @@
-/* hello.c */
 #include <stdio.h>
+#include <string.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+int main() {
+    char str1[100], str2[100], result[200];
+    int i, j, len;
+
+    // Ввод строк
+    printf("Введите первую строку: ");
+    scanf("%s", str1);
+    printf("Введите вторую строку: ");
+    scanf("%s", str2);
+
+    // Получение длины строк
+    len = strlen(str1);
+
+    // Формирование новой строки
+    j = 0;
+    for (i = 0; i < len; i++) {
+        if (i % 2 == 0) {
+            result[j] = str1[i];
+        } else {
+            result[j] = str2[i];
+        }
+        j++;
+    }
+    result[j] = '\0'; // Добавление завершающего нулевого символа для обозначения конца строки
+
+    // Вывод результата
+    printf("Результирующая строка: %s\n", result);
+
+    return 0;
 }
